da37ae7121ca17faf21b62c310343d907bdd17c4
martinstyve <martin.styve@icloud.com>
2026-03-13 14:02:40 +0100

    slight update PaceRange record

--- src/PaceRange.hs
+++ src/PaceRange.hs
@@ -10,8 +10,8 @@ data Zone
 
 data PaceRange = PaceRange
   { name      :: String
-  , minPace   :: Double
-  , maxPace   :: Double
+  , minPace   :: Integer -- sec per k
+  , maxPace   :: Integer -- sec per k
   , intensity :: String
   } deriving (Show, Eq)
 
@@ -20,33 +20,25 @@ type Pace = (Integer, Integer)
 calculatePaces :: VDOT -> [PaceRange]
 calculatePaces vdot = map (calculateZonePace vdot) [minBound .. maxBound]
 
+-- todo: find good pace ranges
 calculateZonePace :: VDOT -> Zone -> PaceRange
 calculateZonePace vdot zone =
   case zone of
-    ShortRep ->
-      PaceRange
-        { name = "Short Length Intervals"
-        , minPace = 0 -- pace per k for +/- 5% of 15k intensity
-        , maxPace = 1 -- pacePerKm $ equivalentPace vdot (CustomDistance 15000)
-        , intensity = "15k pace"
-        }
-    MediumRep ->
-      PaceRange
-        { name = "Medium Length Intervals"
-        , minPace = 0 * vdot
-        , maxPace = 1
-        , intensity = "Half Marathon pace"
-        }
-    LongRep ->
-      PaceRange
-        { name = "Long Length Intervals"
-        , minPace = 0 * vdot
-        , maxPace = 1
-        , intensity = "30k pace"
-        }
+      ShortRep ->
+        PaceRange "Short Intervals" 
+                  (equivalentTime (vdot * 1.05) (CustomDistance 1000))
+                  (equivalentTime (vdot * 0.95) (CustomDistance 1000))
+                  "15k intensity"
+      MediumRep ->
+        PaceRange "Medium Intervals"
+                  (equivalentTime vdot (CustomDistance 1000))
+                  (equivalentTime (vdot * 0.90) (CustomDistance 1000))
+                  "HM intensity"
+      LongRep ->
+        PaceRange "Long Intervals"
+                  (equivalentTime (vdot * 0.85) (CustomDistance 1000))
+                  (equivalentTime (vdot * 0.80) (CustomDistance 1000))
+                  "30k intensity"
 
--- todo : find equivalentPace from VDOT module
--- output correct pace range. tuple (min,sec)?
--- todo sort out RaceTime and other times in vdot, messy
-pacePerKm :: RaceTime -> RaceDistance -> Pace
-pacePerKm time distance = undefined
+-- pacePerKm :: RaceTime -> RaceDistance -> Pace
+-- pacePerKm time distance = undefined
