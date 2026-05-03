2e9faa44ff3b8e4eb5d045423a7b8e263f3490b6
martinstyve <martin.styve@icloud.com>
2026-05-03 14:54:03 +0200

    docs: Haddock for app module 💸

--- src/App.hs
+++ src/App.hs
@@ -3,6 +3,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators     #-}
 
+{-| Module      : App
+Description : Serve the calculator web application
+
+Defines the Servant API, request validation, and page rendering flow for
+the web app
+-}
 module App where
 
 import           Data.Text                (Text)
@@ -13,29 +19,38 @@ import           Servant.HTML.Lucid       (HTML)
 
 import           Html                     (index, indexMaybeError, resultPage)
 import           PaceRange                (calculatePaces)
+import           RunTime                  as RT ( RunTime, runTimeToSec, secToRunTime )
 import           Parser                   as P
 import           RaceDistance             as RD
-import           RunTime                  as RT
 import           VDOT
 
+-- | Application-level errors for request handling and result generation
+--
+-- Constructors:
+--
+-- * @MissingRequiredInput@ - one or more required query parameters are missing
+-- * @InputParseError@ - a query parameter failed validation
+-- * @VdotCalculationError@ - the calculator could not produce a result table
 data AppError
   = MissingRequiredInput
   | InputParseError InputError
   | VdotCalculationError VDOTError
 
+-- | Convert an application error to display text
 appErrorText :: AppError -> Maybe Text
 appErrorText MissingRequiredInput       = Nothing
 appErrorText (InputParseError err)      = Just (P.inputErrorText err)
 appErrorText (VdotCalculationError err) = Just (vdotErrorText err)
 
+-- | Query parameter wrapper for the time input
 newtype TimeParam =
   TimeParam Text
 
--- https://hackage-content.haskell.org/package/http-api-data-0.7/docs/Web-HttpApiData.html
 instance FromHttpApiData TimeParam where
   parseUrlPiece :: Text -> Either Text TimeParam
   parseUrlPiece = Right . TimeParam
 
+-- | Query parameter wrapper for the selected race distance
 newtype DistanceParam =
   DistanceParam Text
 
@@ -43,6 +58,7 @@ instance FromHttpApiData DistanceParam where
   parseUrlPiece :: Text -> Either Text DistanceParam
   parseUrlPiece = Right . DistanceParam
 
+-- | Query parameter wrapper for the optional custom distance value
 newtype CustomDistanceParam =
   CustomDistanceParam Text
 
@@ -50,21 +66,24 @@ instance FromHttpApiData CustomDistanceParam where
   parseUrlPiece :: Text -> Either Text CustomDistanceParam
   parseUrlPiece = Right . CustomDistanceParam
 
--- https://docs.servant.dev/en/latest/tutorial/ApiType.html
+-- | Servant API for the calculator home page and result page
 type API
   = Get '[ HTML] (Html ()) :<|> "result" :> QueryParam "time" TimeParam :> QueryParam
-      "dist" DistanceParam :> QueryParam "customDist" CustomDistanceParam :> Get '[ HTML]
-      (Html ())
+      "dist" DistanceParam :> QueryParam "customDist" CustomDistanceParam :> Get '[ HTML] (Html ())
 
+-- | Proxy value for the application API
 api :: Proxy API
 api = Proxy
 
+-- | Application server implementation
 server :: Server API
 server = homeHandler :<|> resultHandler
 
+-- | Render the home page
 homeHandler :: Handler (Html ())
 homeHandler = return Html.index
 
+-- | Validate query parameters and render either the result page or an error page
 resultHandler :: Maybe TimeParam -> Maybe DistanceParam -> Maybe CustomDistanceParam -> Handler (Html ())
 resultHandler maybeTime maybeDist maybeCustomDist =
   case validateParams maybeTime maybeDist maybeCustomDist of
@@ -74,6 +93,7 @@ resultHandler maybeTime maybeDist maybeCustomDist =
         Left err   -> return (Html.indexMaybeError (appErrorText err))
         Right page -> return page
 
+-- | Validate the query parameters from the request
 validateParams :: Maybe TimeParam -> Maybe DistanceParam -> Maybe CustomDistanceParam -> Either AppError (RT.RunTime, RaceDistance)
 validateParams (Just (TimeParam timeText)) (Just (DistanceParam distChoice)) maybeCustomDist =
   case P.parseTime timeText of
@@ -84,9 +104,11 @@ validateParams (Just (TimeParam timeText)) (Just (DistanceParam distChoice)) may
           Right raceDistance -> Right (runTime, raceDistance)
 validateParams _ _ _ = Left MissingRequiredInput
 
+-- | Extract raw custom distance text from its wrapper
 unwrapCustomDist :: CustomDistanceParam -> Text
 unwrapCustomDist (CustomDistanceParam customDistText) = customDistText
 
+-- | Build the result page for a valid time and distance combination
 buildResultPage :: RT.RunTime -> RD.RaceDistance -> Either AppError (Html ())
 buildResultPage runTime raceDistance =
   case raceTableOrError of
@@ -103,9 +125,11 @@ buildResultPage runTime raceDistance =
 
     intervalPaces = calculatePaces vdot
 
+-- | Servant application entry point
 app :: Application
 app = serve api server
 
+-- | Start the web application on port 6767
 startApp :: IO ()
 startApp = do
   putStrLn "visit http://localhost:6767"
